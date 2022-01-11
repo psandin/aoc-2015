57bc1bdc4eb0b541ea7e311419fefb5670b9b94a
Prime <isabel@thesandins.net>
2022-01-10 22:34:12 -0800

    Day 5 part 1

--- Day-05/part_1.rb
+++ Day-05/part_1.rb
@@ -26,5 +26,30 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
+def nice?(string)
+  !bad_substring(string) && good_vowel_count(string) && double_letter?(string)
+end
+
+def good_vowel_count(string)
+  vowels = %w[a e i o u]
+  string.chars.select { |e| vowels.any? { |v| v == e } }.count > 2
+end
+
+def double_letter?(string)
+  (0..string.length - 1).each do |i|
+    return true if string.slice(i, 2).chars.reduce(:==) == true
+  end
+  false
+end
+
+def bad_substring(string)
+  return true if string.include? 'ab'
+  return true if string.include? 'cd'
+  return true if string.include? 'pq'
+  return true if string.include? 'xy'
+
+  false
+end
+
 raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+puts raw_lines.select { |e| nice?(e) }.count
