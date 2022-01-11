bb4efead69d04c841c5766f9709616a30243f845
Prime <isabel@thesandins.net>
2022-01-10 23:23:13 -0800

    Day 5 part 2

--- Day-05/part_2.rb
+++ Day-05/part_2.rb
@@ -26,5 +26,27 @@ def slurp(path)
   input_str.split(/\n/)
 end
 
+def nice?(string)
+  double_pair?(string) && spaced_pair?(string)
+end
+
+def double_pair?(string)
+  (0..string.length - 3).each do |i|
+    target_str = string.slice(i, 2)
+    (i + 2..string.length - 1).each do |j|
+      return true if target_str == string.slice(j, 2)
+    end
+  end
+  false
+end
+
+def spaced_pair?(string)
+  (0..string.length - 2).each do |i|
+    triple = string.slice(i, 3).chars
+    return true if triple[0] == triple[2]
+  end
+  false
+end
+
 raw_lines = slurp($args[:file])
-puts raw_lines.to_s
+puts raw_lines.select { |e| nice?(e) }.count
